310244f9cabf841227927ac24285ffff25d4be2f
bikoil <bikoil@proton.me>
2024-08-08 02:43:08 +0300

    Added a script and more documentation

---
+++ install.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+# Function to build the package
+build_pkg() {
+	echo "-> Changing file name..."
+	mv ralsay.lua ralsay
+	echo "-> Making file executable..."
+	chmod +x ralsay
+	echo "-> Copying file to '/usr/local/bin... Requires SUDO permissions'"
+	sudo cp ralsay /usr/local/bin
+	echo "-! Ralsay package installed! Thank you for installing my package."
+}
+# Function to check the OS
+check_os() {
+    if [ -f /etc/os-release ]; then
+        # Check for Fedora or Debian-based systems
+        . /etc/os-release
+        case "$ID" in
+            fedora)
+                echo "-> Installing lua via dnf..."
+                sudo dnf install lua
+                build_pkg
+                ;;
+            debian)
+                echo "-> Installing lua via apt..."
+                sudo apt install lua
+                build_pkg
+                ;;
+            ubuntu)
+                echo "-> Installing lua via apt"
+                sudo apt install lua 
+                build_pkg
+                ;;
+            *)
+                echo "Unknown Linux distribution, please build it by hand"
+                ;;
+        esac
+    elif [ -f /etc/freebsd-version ]; then
+        # Check for FreeBSD
+        echo "FreeBSD"
+    elif [ "$(uname)" = "Darwin" ]; then
+        # Check for macOS
+        echo "-> Installing lua via brew..."
+        brew install lua
+        build_pkg
+    else
+        echo "Unknown operating system, please build it by hand"
+    fi
+}
+
+# Call the function
+check_os
+
